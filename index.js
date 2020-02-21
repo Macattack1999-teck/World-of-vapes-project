a422d11779585dd3438c968e1d4653bcf88d5c70
Michale-Cortez <CortezDevelopement@gmail.com>
2020-02-20 21:40:58 -0700

    Initial Commit -- I made a modal popup window and styled my navBar

---
+++ index.js
@@ -0,0 +1,11 @@
+// let modal = document.querySelector("#myModal");
+//   let yesBtn = document.querySelector(".ysBtn");
+//   let noBtn = document.querySelector(".nBtn")
+
+//   yesBtn.addEventListener("click", function() {
+//     modal.style.display = "none";
+//   })
+
+//   noBtn.addEventListener("click", function() {
+//     location.href = "https://www.webmd.com/lung-cancer/default.htm";
+//   })
